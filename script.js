74d27bd02d03eafd6d5402343aad6f98cd71efff
rkp8255 <127007355+rkp8255@users.noreply.github.com>
2025-01-29 16:36:12 +0530

    added scripts.js

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const sections = document.querySelectorAll('section');
+
+  const options = {
+    root: null,
+    threshold: 0.1,
+    rootMargin: '0px'
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  sections.forEach(section => {
+    observer.observe(section);
+  });
+});
